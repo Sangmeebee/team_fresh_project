d91d5f4b47bf8f69196ffabb273e770f10bff353
Sangmeebee <apfhdznzl@gmail.com>
2022-11-21 22:54:53 +0900

    feat: 네트워크 모듈을 생성후 네트워크 통신에 필요한 인스턴스를 바인딩 그래프에 추가한다

---
+++ data/src/main/java/com/sangmeebee/teamfreshproject/data/di/NetworkModule.kt
@@ -0,0 +1,49 @@
+package com.sangmeebee.teamfreshproject.data.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Converter
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal object NetworkModule {
+
+    private const val TEAM_FRESH_BASE_URL = "https://yhapidev.teamfresh.co.kr/"
+
+    @Singleton
+    @Provides
+    fun provideConverter(): Converter.Factory = GsonConverterFactory.create()
+
+    @Singleton
+    @Provides
+    fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor =
+        HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+
+
+    @Singleton
+    @Provides
+    fun provideOkhttpClient(
+        httpLoggingInterceptor: HttpLoggingInterceptor,
+    ): OkHttpClient =
+        OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(
+        converter: Converter.Factory,
+        okHttpClient: OkHttpClient,
+    ): Retrofit = Retrofit.Builder()
+        .client(okHttpClient)
+        .baseUrl(TEAM_FRESH_BASE_URL)
+        .addConverterFactory(converter)
+        .build()
+}

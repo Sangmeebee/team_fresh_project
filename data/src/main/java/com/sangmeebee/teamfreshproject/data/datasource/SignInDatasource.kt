9154eba9e35834de64ca7b806cd64ae38f1a09bf
Sangmeebee <apfhdznzl@gmail.com>
2022-11-22 03:15:37 +0900

    refactor: 불필요한 코드 제거

--- data/src/main/java/com/sangmeebee/teamfreshproject/data/datasource/SignInDatasource.kt
+++
@@ -1,8 +0,0 @@
-package com.sangmeebee.teamfreshproject.data.datasource
-
-import com.sangmeebee.teamfreshproject.domain.model.SignInInfo
-import com.sangmeebee.teamfreshproject.domain.model.Token
-
-interface SignInDatasource {
-    suspend fun signIn(signInInfo: SignInInfo): Result<Token>
-}

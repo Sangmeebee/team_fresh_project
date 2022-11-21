386fedc2bc7da20a302ac8441b1b0e93d367a0c0
Sangmeebee <apfhdznzl@gmail.com>
2022-11-21 21:21:19 +0900

    feat: data 모듈을 생성한다

---
+++ data/build.gradle.kts
@@ -0,0 +1,70 @@
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+    id("kotlin-kapt")
+}
+
+android {
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 33
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation(CoroutineConfig.CORE)
+
+    implementation(PagingConfig.PAGING_COMMON)
+
+    NetworkConfig.run {
+        implementation(RETROFIT)
+        implementation(RETROFIT_CONVERTER)
+        implementation(platform(OKHTTP_BOM))
+        implementation(OKHTTP)
+        implementation(OKHTTP_LOGGING_INTERCEPTOR)
+    }
+
+    RoomConfig.run {
+        implementation(ROOM_RUNTIME)
+        kapt(ROOM_COMPILER)
+        implementation(ROOM_KTX)
+        implementation(ROOM_PAGING)
+    }
+
+    implementation(ConverterConfig.GSON)
+
+    HiltConfig.run {
+        implementation(ANDROID)
+        kapt(COMPILER)
+    }
+
+    UnitTestConfig.run {
+        testImplementation(JUNIT)
+        testImplementation(ANDROIDX_JUNIT_KTX)
+        testImplementation(ANDROIDX_CORE_KTX)
+        testImplementation(TRUTH)
+        testImplementation(ROBOLECTRIC)
+        testImplementation(COROUTINE_TEST)
+        testImplementation(MOCKK)
+        testImplementation(MOCK_WEBSERVER)
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,4 @@
 rootProject.name = "TeamFreshProject"
 include(":app")
 include(":domain")
+include(":data")

0fb16ee70a8985925fe3fe6bfa07c3fed448bbd7
Sangmeebee <apfhdznzl@gmail.com>
2022-11-22 02:19:36 +0900

    test: 회원가입 데이터소스 테스트 코드 구현 & 테스트

---
+++ data/src/test/java/com/sangmeebee/teamfreshproject/data/datasource/SignInDatasourceImplTest.kt
@@ -0,0 +1,84 @@
+package com.sangmeebee.teamfreshproject.data.datasource
+
+import com.google.common.truth.Truth.assertThat
+import com.sangmeebee.teamfreshproject.data.model.SignInEntity
+import com.sangmeebee.teamfreshproject.data.model.TokenEntity
+import com.sangmeebee.teamfreshproject.data.service.SignInAPI
+import com.sangmeebee.teamfreshproject.domain.datasource.SignInDatasource
+import com.sangmeebee.teamfreshproject.domain.util.IllegalIdException
+import com.sangmeebee.teamfreshproject.domain.util.IllegalPasswordException
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class SignInDatasourceImplTest {
+
+    @get:Rule
+    val mainDispatcherRule = MainDispatcherRule()
+
+    private lateinit var signInAPI: SignInAPI
+    private lateinit var signInDatasource: SignInDatasource
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Before
+    fun setUp() {
+        signInAPI = mockk()
+        signInDatasource = SignInDatasourceImpl(signInAPI = signInAPI, dispatcher = mainDispatcherRule.testDispatcher)
+    }
+
+    @Test
+    fun `회원가입을 성공하면 토큰을 반환한다`() = runTest {
+        // given
+        val signInEntity = SignInEntity(id = "appdev", password = "Timf1234")
+        val tokenEntity = TokenEntity(
+            success = true,
+            code = 0,
+            message = "성공하였습니다.",
+            token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIyNjgiLCJ1c2VyTG9naW5JZCI6ImFwcGRldiIsInVzZXJObSI6IuyVse2FjOyKpO2KuOqzhOyglSIsInVzZXJTZXEiOjI2OCwidXNlck5jbm0iOiLslbHthYzsiqTtirjqs4TsoJUiLCJyb2xlcyI6W3siYXV0aG9yaXR5IjoiREVGQVVMVCJ9XSwiaWF0IjoxNjY5MDM5MDQ3LCJleHAiOjkyMjMzNzIwMzY4NTQ3NzV9.qXYuNk-7KdMkOFcbWfnUT_Wo4UuzC08WOY6su4I-ysQ"
+        )
+        coEvery { signInAPI.signIn(signInEntity) } returns tokenEntity
+        // when
+        val actual = signInDatasource.signIn(signIn = signInEntity.toDomain())
+        // then
+        assertThat(actual.isSuccess).isTrue()
+        actual.onSuccess { assertThat(it).isEqualTo(tokenEntity.toDomain()) }
+    }
+
+    @Test
+    fun `아이디를 잘못 입력하면 해당 예외가 발생한다`() = runTest {
+        // given
+        val signInEntity = SignInEntity(id = "WrongUser", password = "Timf1234")
+        val tokenEntity = TokenEntity(
+            success = false,
+            code = -1000,
+            message = "존재하지 않는 회원입니다."
+        )
+        coEvery { signInAPI.signIn(signInEntity) } returns tokenEntity
+        // when
+        val actual = signInDatasource.signIn(signIn = signInEntity.toDomain())
+        // then
+        assertThat(actual.isFailure).isTrue()
+        actual.onFailure { assertThat(it).isInstanceOf(IllegalIdException::class.java) }
+    }
+
+    @Test
+    fun `비밀번호를 잘못 입력하면 해당 예외가 발생한다`() = runTest {
+        // given
+        val signInEntity = SignInEntity(id = "appdev", password = "WrongPassword")
+        val tokenEntity = TokenEntity(
+            success = false,
+            code = -1001,
+            message = "비밀번호가 정확하지 않습니다."
+        )
+        coEvery { signInAPI.signIn(signInEntity) } returns tokenEntity
+        // when
+        val actual = signInDatasource.signIn(signIn = signInEntity.toDomain())
+        // then
+        assertThat(actual.isFailure).isTrue()
+        actual.onFailure { assertThat(it).isInstanceOf(IllegalPasswordException::class.java) }
+    }
+}

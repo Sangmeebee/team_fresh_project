b96430b5fbb6cab8cce04e5ab96af2f445d6b5e6
Sangmeebee <apfhdznzl@gmail.com>
2022-11-23 18:41:36 +0900

    feat: 회원가입 UI Layer 구현

--- app/src/main/java/com/sangmeebee/teamfreshproject/ui/sign_in/SignInFragment.kt
+++ app/src/main/java/com/sangmeebee/teamfreshproject/ui/sign_in/SignInFragment.kt
@@ -1,8 +1,105 @@
 package com.sangmeebee.teamfreshproject.ui.sign_in
 
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.sangmeebee.teamfreshproject.R
 import com.sangmeebee.teamfreshproject.databinding.FragmentSignInBinding
+import com.sangmeebee.teamfreshproject.domain.util.HttpConnectionException
+import com.sangmeebee.teamfreshproject.domain.util.IllegalIdException
+import com.sangmeebee.teamfreshproject.domain.util.IllegalPasswordException
+import com.sangmeebee.teamfreshproject.model.SignInInfoModel
 import com.sangmeebee.teamfreshproject.ui.base.BaseFragment
+import com.sangmeebee.teamfreshproject.util.repeatOnStarted
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.map
 
 @AndroidEntryPoint
-class SignInFragment : BaseFragment<FragmentSignInBinding>(FragmentSignInBinding::inflate)
+class SignInFragment : BaseFragment<FragmentSignInBinding>(FragmentSignInBinding::inflate) {
+
+    private val signInViewModel by viewModels<SignInViewModel>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUpClickListener()
+        setUpObserveUiState()
+        setSwipeRefreshLayout()
+    }
+
+    private fun setUpClickListener() {
+        binding.tvLogin.setOnClickListener {
+            val id = binding.etId.text.toString()
+            val password = binding.etPassword.text.toString()
+            if (id.isBlank()) {
+                showToast(resources.getString(R.string.sign_in_id_empty))
+                return@setOnClickListener
+            }
+            if (password.isBlank()) {
+                showToast(resources.getString(R.string.sign_in_password_empty))
+                return@setOnClickListener
+            }
+            val signInInfoModel = SignInInfoModel(
+                id = id,
+                password = password
+            )
+            signInViewModel.signIn(signInInfoModel)
+        }
+
+        binding.tvFindId.setOnClickListener {
+            showToast(resources.getString(R.string.sign_in_click_find_id))
+        }
+
+        binding.tvFindPw.setOnClickListener {
+            showToast(resources.getString(R.string.sign_in_click_find_password))
+        }
+
+        binding.tvSignUp.setOnClickListener {
+            showToast(resources.getString(R.string.sign_in_click_sign_up))
+        }
+
+        binding.ivKakaoLogin.setOnClickListener {
+            showToast(resources.getString(R.string.sign_in_click_sign_in_kakao))
+        }
+    }
+
+    private fun setSwipeRefreshLayout() {
+        binding.srlLoading.isEnabled = false
+    }
+
+    private fun setUpObserveUiState() {
+        observeError()
+        observeIsSignIn()
+        observeIsLoading()
+    }
+
+    private fun observeError() = repeatOnStarted {
+        signInViewModel.uiState.map { it.error }.distinctUntilChanged().collectLatest { throwable ->
+            if (throwable != null) {
+                when (throwable) {
+                    is IllegalIdException -> showToast(resources.getString(R.string.sign_in_id_error))
+                    is IllegalPasswordException -> showToast(resources.getString(R.string.sign_in_password_error))
+                    is HttpConnectionException -> showToast(resources.getString(R.string.common_network_error))
+                    else -> showToast(resources.getString(R.string.sign_in_common_error))
+                }
+                signInViewModel.fetchError(null)
+            }
+        }
+    }
+
+    private fun observeIsLoading() = repeatOnStarted {
+        signInViewModel.uiState.map { it.isLoading }.distinctUntilChanged().collectLatest {
+            binding.srlLoading.isRefreshing = it
+        }
+    }
+
+    private fun observeIsSignIn() = repeatOnStarted {
+        signInViewModel.uiState.map { it.isSignIn }.distinctUntilChanged().collectLatest { isSignIn ->
+            if (isSignIn) {
+                findNavController().navigate(R.id.action_signInFragment_to_home)
+            }
+        }
+    }
+}
--- domain/src/main/java/com/sangmeebee/teamfreshproject/domain/util/Codes.kt
+++ domain/src/main/java/com/sangmeebee/teamfreshproject/domain/util/Codes.kt
@@ -1,5 +1,4 @@
 package com.sangmeebee.teamfreshproject.domain.util
 
-const val SUCCESS_CODE = 0
 const val ID_EXCEPTION_CODE = -1000
 const val PASSWORD_EXCEPTION_CODE = -1001
